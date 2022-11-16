c9147c4e07296ffb27c5bba33e151f2042b4c227
blackbeans-1301 <blackbeans.1301@gmail.com>
2022-11-16 15:24:19 +0700

    add react native tracck player

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import useCachedResources from "./hooks/useCachedResources"
 import useColorScheme from "./hooks/useColorScheme"
 import Navigation from "./navigation"
 import PlayerWidget from "./components/PlayerWidget"
-import React, { useCallback, useContext, useState } from "react"
+import React, { useCallback, useContext, useEffect, useState } from "react"
 import { NavigationContainer } from "@react-navigation/native"
 
 import { Amplify, Auth, auth0SignInButton } from "aws-amplify"
@@ -16,19 +16,63 @@ import { AppContext } from "./AppContext"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import SignUpScreen from "./screens/home/SignUpScreen"
+import TrackPlayer, { Capability, State } from "react-native-track-player"
 
 Amplify.configure(config)
 
 const Stack = createNativeStackNavigator()
 
+const track1 = {
+  url: "http://api.mp3.zing.vn/api/streaming/audio/ZWBIF86E/320", // Load media from the network
+  title: "Avaritia",
+  artist: "deadmau5",
+  album: "while(1<2)",
+  genre: "Progressive House, Electro House",
+  date: "2014-05-20T07:00:00+00:00", // RFC 3339
+  artwork: "http://example.com/cover.png", // Load artwork from the network
+  duration: 402, // Duration in seconds
+}
+
+async function setup() {
+  await TrackPlayer.setupPlayer({})
+  await TrackPlayer.updateOptions({
+    capabilities: [
+      Capability.Play,
+      Capability.Pause,
+      Capability.SkipToNext,
+      Capability.SkipToPrevious,
+      Capability.Stop,
+      Capability.SeekTo,
+    ],
+    compactCapabilities: [Capability.Play, Capability.Pause],
+  })
+
+  await TrackPlayer.add([track1])
+
+  const state = await TrackPlayer.getState()
+  if (state === State.Playing) {
+    console.log("The player is playing")
+  }
+
+  let trackIndex = await TrackPlayer.getCurrentTrack()
+  let trackObject = await TrackPlayer.getTrack(trackIndex)
+  console.log(`Title: ${trackObject.title}`)
+
+  console.log("fhsaifh")
+
+  TrackPlayer.play()
+}
+
 function App() {
+  setup()
+
   const { hasUser } = useContext(AppContext)
   console.log(hasUser)
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName={hasUser === "" ? "SignInScreen" : "AppScreen"}
+          initialRouteName={hasUser !== "" ? "SignInScreen" : "AppScreen"}
         >
           <Stack.Screen
             name="SignInScreen"
